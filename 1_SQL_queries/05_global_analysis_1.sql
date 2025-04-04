f2c4bd89d3a0d2a30eaef76b609d67351ae080cd
Tyron-patterns <tyron.dlt@hotmail.it>
2025-04-04 20:16:56 +0200

    Update 05_global_analysis_1.sql

--- 1_SQL_queries/05_global_analysis_1.sql
+++ 1_SQL_queries/05_global_analysis_1.sql
@@ -72,46 +72,17 @@
 
 
 /*---------------------------------------------------------------------------------------------------------------------------------------------
-🔴5.B) CALCULATE AND RANK THE AVERAGE TEMPERATURE CHANGE FOR EACH COUNTRY OVER TIME. HIGHLIGHT THE TOP 15 FASTEST-WARMING COUNTRIES AND RETRIEVE THE FULL DATASET IF NEEDED
+🔴3.C) Identify the country with the highest temperature increase. Optionally, filter for the last century (year > 1900)
 ---------------------------------------------------------------------------------------------------------------------------------------------*/
 
-	🔵%.B.1) Version A: Regression for all years
-
-	select country, 
-			round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
-	from (select country, 
-				extract(year from dt) as year, 
-				round(avg(averagetemp)::numeric,2) as avg_temp_per_year
-		from global_t 
-		where averagetemp is not null 								
-		and country not in ('North America', 'South America', 'Africa', 'Europe', 'Australia', 'Asia')
-		group by year, country) 
-	group by country 
-	having regr_slope(avg_temp_per_year, year) is not null
-	order by temp_increase desc;
-
-
-	-- include to retrieve only the 15 fastest warming countries
-
-	order by temp_increase desc 
-	limit 15;
-
-	-- include to retrieve the single fastest warming country
-
-	order by temp_increase desc 
-	limit 1;
-
-
-	🔵5.B.2)Version B: Regression post-1900 (used in main analysis)
-
-	select country, 
-			round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
-	from (select extract(year from dt) as year, 
-				round(avg(averagetemp)::numeric,2) as avg_temp_per_year,
-				country
-			from global_t 
-			group by year, country) 
-	where year > '1900' -- remove to retrieve ranking for the entire time span
-	group by country
-	having regr_slope (avg_temp_per_year, year) is not null
-	order by temp_increase desc;
+select country, --highest temp increase overtime in 1900
+round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
+from (select extract(year from dt) as year, 
+round(avg(averagetemp)::numeric,2) as avg_temp_per_year,
+country
+from global_t 
+group by year, country) 
+where year > '1900' -- remove to retrieve ranking for the entire time span
+group by country
+having regr_slope (avg_temp_per_year, year) is not null
+order by temp_increase desc;
