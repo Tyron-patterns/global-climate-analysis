3cdd66ec46471deb81741ef30f7b93760eb9db47
Tyron-patterns <tyron.dlt@hotmail.it>
2025-04-04 20:01:25 +0200

    Update 02_global_analysis.sql

--- 1_SQL_queries/02_global_analysis.sql
+++ 1_SQL_queries/02_global_analysis.sql
@@ -11,10 +11,10 @@
 							===========================================================*/
 
 /*---------------------------------
-🔴2.A) CHECK FOR MEANINGFUL VALUES
+🔴3.A) CHECK FOR MEANINGFUL VALUES
 ----------------------------------*/
 
-	🔵2.A.1) retrieves important values on a global level
+	🔵3.A.1) retrieves important values on a global level
 
 	select min(averagetemp) as min, 
 			round(max(averagetemp)::numeric, 3) as max,--checks for important values 
@@ -26,7 +26,7 @@
 	from global_t;
 
 	
-	🔵2.A.2) retrieves average of important values on a country level
+	🔵3.A.2) retrieves average of important values on a country level
 
 	select round(avg(min_temp)::numeric,3) as avg_min, -- average important values on a country level
 			round(avg(max_temp)::numeric,3) as avg_max, 
@@ -42,7 +42,7 @@
 	order by country);
 
 
-	🔵2.A.i) checks the magnitud of uncertainties related to each measures
+	🔵3.A.i) checks the magnitud of uncertainties related to each measures
 
 	select count(*) 
 	from global_t 
@@ -124,80 +124,3 @@
 
 
 
-/*---------------------------------------------------------------------------------------------------------------------------------------------
-🔴3.C) ANALYZE THE GLOBAL TEMPERATURE TREND OVER TIME. IF THE DATASET INCLUDED CONTINENTS, THE SAME METHOD COULD BE USED FOR CONTINENT-BASED TRENDS
----------------------------------------------------------------------------------------------------------------------------------------------*/
-
-	🔵3.C.1) retrieves the regression slope per country per year 
-
-	select round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
-	from (select extract(year from dt) as year, 
-				round(avg(averagetemp)::numeric,2) as avg_temp_per_year
-	from global_t 
-	group by year);
-
-
-	🔵3.C.2) retrieves the regression slope per continent per year
-
-	select continent, 
-			round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
-	from (select continent, 
-				extract(year from dt) as year, 
-				round(avg(averagetemp)::numeric,2) as avg_temp_per_year
-			from global_land_temp 
-			group by continent, year) 
-	group by continent
-	having regr_slope(avg_temp_per_year, year) is not null
-	order by continent, temp_increase desc;
-
-
-
-/*-------------------------------------------------------------
-🔴3.D) WHICH COUNTRIES HAVE THE MOST MISSING TEMPERATURE DATA?
--------------------------------------------------------------*/
-
-select country, count(country) from global_t 
-where averagetemp is null
-group by country 
-order by count(country) desc;
-
-
-/*---------------------------------------------------------------------------------------------------------------------------------------------
-🔴3.E) WHAT IS THE AVERAGE TEMPERATURE TREND IN EUROPE (NO CONTINENT COLUMNS WAS PROVIDED IN THE TABLE AND EUROPE WAS LISTED AS A COUNTRY) AND 5 COUNTRIES IN EUROPE OVER THE LAST 50 YEARS?
----------------------------------------------------------------------------------------------------------------------------------------------*/
-	
-	🔵3.E.1) retrieves for regression slope of countries per year
-
-	select country, 
-			round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
-	from (select country, 	
-				extract(year from dt) as year, 
-				round(avg(averagetemp)::numeric,2) as avg_temp_per_year
-			from global_t 
-			where country = 'Europe' and extract(year from dt) > '1975' 
-			group by country, year)
-	group by country
-	having regr_slope(avg_temp_per_year, year) is not null
-	order by temp_increase desc;
-
-
-	🔵3.E.2)asses which European countries are in the table
-
-	select distinct(country) 
-	from global_t 
-	order by country; 
-
-	🔵3.E.3) retrieves highest temp increase overtime (in 5 countries)
-
-	select country, 
-			round(regr_slope(avg_temp_per_year, year)::numeric,5) as temp_increase 
-	from (select country, 
-				extract(year from dt) as year, 
-				round(avg(averagetemp)::numeric,2) as avg_temp_per_year
-			from global_t 
-			where country IN ('Albania', 'Belgium', 'Bulgaria', 'Croatia', 'Denmark') 
-			and extract(year from dt) >'1975' 
-			group by country, year) as uropean_countries
-	group by country
-	having regr_slope(avg_temp_per_year, year) is not null
-	order by temp_increase desc;
